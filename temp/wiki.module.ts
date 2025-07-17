d76fde7de27548f95074c45d934f9e9d7eb3aec7
Carlo Nicora <carlo.nicora@gmail.com>
2025-07-17 09:27:24 +0200

    feat: remove temp content

--- temp/wiki.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ArticleModule } from "src/features/wiki/modules/article/article.module";
-import { ArticleVersionModule } from "src/features/wiki/modules/articleversion/articleversion.module";
-
-@Module({
-  imports: [ArticleModule, ArticleVersionModule],
-})
-export class WikiModule {}
